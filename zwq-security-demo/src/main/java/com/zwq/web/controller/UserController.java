622ea94341e37ee727517a3dcdd1593b6f01fcc8
Moriarty <wozhangqiang@163.com>
2017-10-30 10:25:11 +0800

    更改结构

--- zwq-security-demo/src/main/java/com/zwq/web/controller/UserController.java
+++ zwq-security-demo/src/main/java/com/zwq/web/controller/UserController.java
@@ -2,8 +2,8 @@ package com.zwq.web.controller;
 
 
 import com.fasterxml.jackson.annotation.JsonView;
-import dto.User;
-import dto.UserQueryCondition;
+import com.zwq.dto.User;
+import com.zwq.dto.UserQueryCondition;
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
@@ -12,7 +12,6 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
