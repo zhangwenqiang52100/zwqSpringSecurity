5eb39d420975762a5614fed67f48bc0ca5c1f861
zhang wenqiang <zhangwenqiang@gmail.com>
2017-11-07 07:55:11 +0800

    json数据返回成功失败

--- zwq-security-browser/src/main/java/com/zwq/security/browser/BrowserSecurityConfig.java
+++ zwq-security-browser/src/main/java/com/zwq/security/browser/BrowserSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.zwq.security.browser;
 
+import com.zwq.security.browser.authentication.ZwqAuthenticationFailHandler;
+import com.zwq.security.browser.authentication.ZwqAuthenticationSuccessHandler;
 import com.zwq.security.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +17,12 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   private SecurityProperties securityProperties;
+  @Autowired
+  @Qualifier("zwqAuthenticationSuccessHandler")
+  private ZwqAuthenticationSuccessHandler zwqAuthenticationSuccessHandler;
+  @Autowired
+  @Qualifier("zwqAuthenticationFailHandler")
+  private ZwqAuthenticationFailHandler zwqAuthenticationFailHandler;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -25,6 +34,8 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     http.formLogin()
         .loginPage("/authentication/require")
         .loginProcessingUrl("/authentication/form")
+        .successHandler(zwqAuthenticationSuccessHandler)
+        .failureHandler(zwqAuthenticationFailHandler)
         .and()
         .authorizeRequests()
         .antMatchers("/authentication/require",
